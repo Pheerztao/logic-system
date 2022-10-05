03f7492c3741dc8fdd8050a2e2e043946007091b
Pheerztao <faadedokun8@gmail.com>
2022-10-05 14:56:50 +0100

    login-system

--- index.js
+++ index.js
@@ -27,30 +27,77 @@ const connection = mysql.createConnection({
 connection.connect()
 
 app.post(`/create`, (req,res) => {
+
  const firstname = req.body.firstname
  const lastname = req.body.lastname
  const phone = req.body.phone
  const email = req.body.email
  const age = req.body.age
+
  
  const schema = Joi.object({
   firstname: Joi.string().min(4).max(30),
   surnmame:  Joi.string().min(4).max(30).required(),
-  phone: Joi.string().min(11).max(14).requires(),
+  phone: Joi.string().min(11).max(14).required(),
   email: Joi.string().email({minDomainSegments: 2, tlds:{allow: [`com`,  `net`] } }),
-  age: Joi.string().min(1).max(3)
+  age: Joi.string().max(3)
 })
 const { error, value } = schema.validate(req.body);
 
-  if(error != "undefined"){
+  if(error != undefined){
 
     res.status(400).send({
         message: 'validation error'
     })
   }
+ res.status(201).send({
+        message: 'successful',
+        data: req.body
+    })
 
-if()
+ try {
+    connection.query(
+  `select * from customers where email='${email}' or phone='${phone}'`,
+   (err, results, fields) => {
+    if (err) {
+       console.log("1: error: ", err)
+       throw new Error('Please check back, this is on us.')
+ 
+   }
+      
+   if (results.length > 0) {
+    throw new Error ('The email/Phone exists.', 400)
+    }
+
+
+   //create the customer 
+   connection.query(
+   `insert into customers(customer_id,firstname, othernames, phone, email)
+  values('${uuidv5()}','${firstname}','${othername}','${phone}', '${email}',${age} )`,
+        (err, results, fields) => {
+         if (err) {
+            console.log("2: error: ", err)
+      throw new Error("This is on us, pleae try later")
+   }
+
+res.status(201).json({
+        status:true,
+  message: "Account succesfully created",
+        data: req.body
+    })
+     }
+);
+  })
+ }
+ catch (e) {
+
+  res.status(400).json({
+      message: e.message
+  })
+
+}
 })
+
 
 
 
